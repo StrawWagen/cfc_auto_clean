0c9546dfa9204cd025ea3792703dd31ab75ac5b7
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-05-21 12:33:13 -0700

    No spaces in braces

--- lua/autorun/client/cl_autoclean.lua
+++ lua/autorun/client/cl_autoclean.lua
@@ -12,7 +12,7 @@ end
 net.Receive( "CFC_AutoClean_RunCommand", function() 
     local command = net.ReadString()
 
-    if isAllowedCommand[ command ] then
+    if isAllowedCommand[command] then
         notifyPlayer( "The server is running '" .. command .. "' on your client.." )
         RunConsoleCommand( command )
     end
