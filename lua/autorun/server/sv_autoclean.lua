10df9f4e068441a3d277125b86bda68640fbc416
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-08-20 23:43:58 -0400

    Oopsies

--- lua/autorun/server/sv_autoclean.lua
+++ lua/autorun/server/sv_autoclean.lua
@@ -20,20 +20,20 @@ local function notifyPlayers( notification )
 end
 
 local clearingServerMessages = {
-    "[CFC - AutoClean] Scrubbing the ethernet.."
-    "[CFC - AutoClean] Dusting the heatsinks.."
-    "[CFC - AutoClean] Applying more thermal paste.."
-    "[CFC - AutoClean] Tidying.."
-    "[CFC - AutoClean] Replacing internet juice.."
-    "[CFC - AutoClean] Washing threads.."
-    "[CFC - AutoClean] Cleaning forks.."
-    "[CFC - AutoClean] Downloading more RAM.."
-    "[CFC - AutoClean] Updating drivers.."
-    "[CFC - AutoClean] Installing more antivirus.."
-    "[CFC - AutoClean] Brushing teeth.."
-    "[CFC - AutoClean] Cleaning behind the ears.."
-    "[CFC - AutoClean] Vacuuming.."
-    "[CFC - AutoClean] Taking vitamins.."
+    "[CFC - AutoClean] Scrubbing the ethernet..",
+    "[CFC - AutoClean] Dusting the heatsinks..",
+    "[CFC - AutoClean] Applying more thermal paste..",
+    "[CFC - AutoClean] Tidying..",
+    "[CFC - AutoClean] Replacing internet juice..",
+    "[CFC - AutoClean] Washing threads..",
+    "[CFC - AutoClean] Cleaning forks..",
+    "[CFC - AutoClean] Downloading more RAM..",
+    "[CFC - AutoClean] Updating drivers..",
+    "[CFC - AutoClean] Installing more antivirus..",
+    "[CFC - AutoClean] Brushing teeth..",
+    "[CFC - AutoClean] Cleaning behind the ears..",
+    "[CFC - AutoClean] Vacuuming..",
+    "[CFC - AutoClean] Taking vitamins..",
     "[CFC - Antibullying] Calling moms..",
     "[CFC - Antiminge] Removing users..",
     "[CFC - Crimial] Stealing copies of phatmania..",
